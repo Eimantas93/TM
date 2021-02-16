b96200000a2f6b750f12865672a7213df41347e3
Eimantas93 <eimantasbakys@gmail.com>
2021-02-16 18:18:32 +0200

    Implemented database connection and registration with hashed password

--- application.py
+++ application.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template, request
 import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 app = Flask(__name__)
-
-connection = sqlite3.connect('data.db')
-con = connection.cursor()
 
 
 @app.route("/")
 def hello():
-    return render_template("layout.html")
+    return render_template("test.html")
 
 
 @app.route("/register", methods=["POST"])
 def register():
+
+    connection = sqlite3.connect('data.db')
     username = request.form.get("username")
     password = request.form.get("password")
-    con.execute("INSERT INTO users (name, hash) VALUES(?, ?)",
-                (username, password))
 
+    hashed_password = generate_password_hash(password)
+
+    connection.execute("INSERT INTO users (name, hash) VALUES(?, ?)",
+                       (username, hashed_password))
     connection.commit()
     connection.close()
+
+    return render_template("layout.html")
