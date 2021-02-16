97cb13ce85f27cba244ef2e65e8eb26742c45783
Eimantas93 <eimantasbakys@gmail.com>
2021-02-16 17:29:15 +0200

    Updated application py
    Testing sqlite queries

--- application.py
+++ application.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
 
-conn = sqlite3.connect('example.db')
+connection = sqlite3.connect('data.db')
+con = connection.cursor()
 
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    con.execute("INSERT INTO users (name, hash) VALUES(?, ?)",
+                (username, password))
+
+    connection.commit()
+    connection.close()
+    return render_template("test.html", username=username, password=password)
